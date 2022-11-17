ba178bac58934a11d690973f21e06f79f607ddb9
ugmw96 <ugmw97@gmail.com>
2022-11-17 14:31:48 +0530

    Go basic concepts

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Wellcome to our game ")
+	
+	var fnumber int
+	fmt.Print("Enter Fist Number");
+	fmt.Scan(&fnumber)
+
+	var snumber int
+	fmt.Print("Enter Seconds Number");
+	fmt.Scan(&snumber);
+
+	var total int = fnumber+ snumber
+
+	fmt.Printf("Fist Number: %v Seconds Number: %v\n Total is: %v", fnumber, snumber, total)
+	
 }
